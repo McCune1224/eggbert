2b26976eab9ffc13ecc054c698377d040f6d7b08
McCune1224 <alexmccune1224@gmail.com>
2025-02-22 11:13:23 -0500

    project structure cleanup

--- scenes/combat/CombatOpponent.cs
+++ scenes/combat/CombatOpponent.cs
@@ -3,15 +3,21 @@ using System;
 
 public partial class CombatOpponent : Node2D
 {
+    private PackedScene _bullet;
     [Export]
     private Sprite2D _Sprite;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        _bullet = GD.Load<PackedScene>("res://scenes/combat/bullets/bullet.tscn");
+        _bullet.Instantiate();
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+        // 2π (full rotation) per second
+        float rotationSpeed = Mathf.Tau;  // Tau is 2π in Godot
+        Rotate(rotationSpeed * (float)delta);
     }
 }
