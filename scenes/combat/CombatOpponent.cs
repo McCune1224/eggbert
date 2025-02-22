b77598daba86ff46ad014a1a8abbc1b142bb458b
McCune1224 <alexmccune1224@gmail.com>
2025-02-22 13:07:40 -0500

    bullet render

--- scenes/combat/CombatOpponent.cs
+++ scenes/combat/CombatOpponent.cs
@@ -3,21 +3,28 @@ using System;
 
 public partial class CombatOpponent : Node2D
 {
-    private PackedScene _bullet;
+    private Node2D _bulletScene;
+    private Node2D[] _bulletPool = new Node2D[1000];
     [Export]
     private Sprite2D _Sprite;
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
-        _bullet = GD.Load<PackedScene>("res://scenes/combat/bullets/bullet.tscn");
-        _bullet.Instantiate();
+        _bulletScene = (Node2D)GD.Load<PackedScene>("res://scenes/combat/bullets/bullet.tscn").Instantiate();
     }
-
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
         // 2π (full rotation) per second
         float rotationSpeed = Mathf.Tau;  // Tau is 2π in Godot
         Rotate(rotationSpeed * (float)delta);
+        Fire();
+    }
+
+    public void Fire()
+    {
+        Node2D bullet = (Node2D)_bulletScene.Duplicate();
+        bullet.GlobalPosition = GlobalPosition; // Or use Position for local coordinates
+        GetTree().Root.AddChild(bullet); // Or use GetParent().AddChild(bullet) to add to current parent
     }
 }
--- scenes/combat/bullets/DefaultBullet.cs
+++ scenes/combat/bullets/DefaultBullet.cs
@@ -6,10 +6,21 @@ public partial class DefaultBullet : Area2D
     // Called when the node enters the scene tree for the first time.
     public override void _Ready()
     {
+        GD.Print("Whats good Bullet exist");
     }
 
     // Called every frame. 'delta' is the elapsed time since the previous frame.
     public override void _Process(double delta)
     {
+    }
+
+    // Check for collision with the player
+    public void _on_Bullet_body_entered(Node body)
+    {
+        if (body is CombatPlayer)
+        {
+            CombatPlayer player = (CombatPlayer)body;
+            QueueFree();
+        }
     }
 }
